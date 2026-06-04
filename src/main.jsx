540a1aef7086f5dde0e1f1704949f795e3137ec4
G. Still <geronimo@heha.online>
2026-06-04 10:35:47 -0400

    feat: load SuperSwoop animation styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,7 @@ import "./account-actions.css";
 import "./onboarding-fix.css";
 import "./heha-brand-theme.css";
 import "./saved-detail-cleanup.css";
+import "./super-swoop.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<StrictMode><App /></StrictMode>);
